28bd62fbac08f3aefee48b960120f0ee142531a0
Manuel Romero <115268570+mromrey587@users.noreply.github.com>
2025-02-20 10:39:14 +0100

    actualizar login

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,47 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatMenuModule } from '@angular/material/menu';
 import { CommonModule } from '@angular/common';
 import { BDService } from '../../services/bd.service';
 @Component({
   selector: 'app-navbar',
-  imports: [
-    RouterModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatMenuModule,
-    CommonModule,
-  ],
+  imports: [RouterModule, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
 export class NavbarComponent implements OnInit {
-  usuario: any = {};
+  usuario: any = null; // Inicializa como null
 
   constructor(private router: Router, private authService: BDService) {}
 
   ngOnInit() {
     // Verificar si hay un usuario autenticado en localStorage
     if (typeof window !== 'undefined') {
-      this.authService.getAuthenticatedUser().subscribe(
-        (data) => {
-          if (data.result === 'success') {
-            this.usuario = data;
+      if (localStorage.getItem('jwt')) {
+        // Si hay un JWT, obtener los datos del usuario
+        this.authService.getAuthenticatedUser().subscribe(
+          (data) => {
+            if (data.result === 'success') {
+              this.usuario = data; // Establecer usuario si autenticado
+            }
+          },
+          (error) => {
+            console.error('Error obteniendo usuario', error);
           }
-        },
-        (error) => {
-          console.error('Error obteniendo usuario', error);
-        }
-      );
+        );
+      }
     }
   }
 
   cerrarSesion() {
     // Eliminar usuario de localStorage y recargar la página
     localStorage.removeItem('jwt');
-    this.usuario = null;
+    this.usuario = null; // Restablecer estado del usuario
     this.router.navigate(['/home']);
   }
 }
