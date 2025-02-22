5af18fba015e7f84c410203ba0221a794db6ea00
Manuel Romero <115268570+mromrey587@users.noreply.github.com>
2025-02-22 13:38:21 +0100

    No te deja inscribirte en dos cursos a la vez

--- frontend/src/app/components/inscripcion/inscripcion.component.ts
+++ frontend/src/app/components/inscripcion/inscripcion.component.ts
@@ -3,6 +3,7 @@ import { BDService } from '../../services/bd.service';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { DatosAltaInscripcion } from '../../interfaces/datosAltaInscripcion';
+import { Inscripcion } from '../../interfaces/inscripcion';
 
 @Component({
   selector: 'app-inscripcion',
@@ -13,13 +14,14 @@ import { DatosAltaInscripcion } from '../../interfaces/datosAltaInscripcion';
 export class InscripcionComponent implements OnInit {
   curso: any = {};
   centro: any = {};
+  inscripciones: Inscripcion[] = [];
   mensaje: string = '';
   usuario: any = {};
   idCurso: number | null = null;
 
   constructor(
     private route: ActivatedRoute,
-    private cursoService: BDService,
+    private bdService: BDService,
     private router: Router
   ) {}
 
@@ -36,7 +38,7 @@ export class InscripcionComponent implements OnInit {
   }
 
   cargarCurso(): void {
-    this.cursoService.getCursoById(this.idCurso!).subscribe(
+    this.bdService.getCursoById(this.idCurso!).subscribe(
       (data) => {
         console.log('Curso recibido', data);
         this.curso = {
@@ -45,17 +47,15 @@ export class InscripcionComponent implements OnInit {
             ? data.imagen
             : `data:image/jpeg;base64,${data.imagen}`,
         };
-        this.cursoService
-          .getCentroById(this.curso.idCentro)
-          .subscribe((data) => {
-            console.log('Centro recibido', data);
-            this.centro = {
-              ...data,
-              imagen: data.imagen.startsWith('data:image')
-                ? data.imagen
-                : `data:image/jpeg;base64,${data.imagen}`,
-            };
-          });
+        this.bdService.getCentroById(this.curso.idCentro).subscribe((data) => {
+          console.log('Centro recibido', data);
+          this.centro = {
+            ...data,
+            imagen: data.imagen.startsWith('data:image')
+              ? data.imagen
+              : `data:image/jpeg;base64,${data.imagen}`,
+          };
+        });
       },
       (error) => console.error('Error al obtener el curso:', error)
     );
@@ -66,11 +66,12 @@ export class InscripcionComponent implements OnInit {
       // Verifica si el jwt existe en localStorage
       const jwt = localStorage.getItem('jwt');
       if (jwt) {
-        this.cursoService.getAuthenticatedUser().subscribe(
+        this.bdService.getAuthenticatedUser().subscribe(
           (data) => {
             if (data.result === 'success') {
               this.usuario = data;
               console.log(this.usuario);
+              this.cargarInscripcionesUsuario();
             }
           },
           (error) => {
@@ -92,7 +93,7 @@ export class InscripcionComponent implements OnInit {
 
     console.log('Datos enviados a la API:', datos); // Verifica en la consola del navegador
 
-    this.cursoService.crearInscripcion(datos).subscribe({
+    this.bdService.crearInscripcion(datos).subscribe({
       next: (respuesta) => {
         console.log('Respuesta del servidor:', respuesta);
         this.mensaje = respuesta.result;
@@ -103,5 +104,24 @@ export class InscripcionComponent implements OnInit {
         this.mensaje = error.error?.result || 'Error al inscribir';
       },
     });
+  }
+
+  cargarInscripcionesUsuario(): void {
+    if (this.usuario.id) {
+      this.bdService.getInscripcionesPorUsuario(this.usuario.id).subscribe({
+        next: (inscripciones) => {
+          this.inscripciones = inscripciones; // ✅ Guarda las inscripciones
+          console.log('Inscripciones guardadas:', this.inscripciones);
+        },
+        error: (error) => {
+          console.error('Error al obtener inscripciones:', error);
+        },
+      });
+    }
+  }
+  verificarInscripcion(): boolean {
+    return this.inscripciones.some(
+      (inscripcion) => inscripcion.id_curso === this.curso.id
+    );
   }
 }
